3949a756f62c2179f8b4867c85ec5c323475b003
Galin <Galin@DESKTOP-CAL3NOT>
2024-03-11 17:57:07 +0200

    Add project files.

---
+++ Problem05/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+       
+        string inputString = Console.ReadLine();
+
+        
+        foreach (char character in inputString)
+        {
+            
+            if (char.IsDigit(character))
+            {
+                Console.Write(character);
+            }
+        }
+
+        Console.WriteLine();
+
+        foreach (char character in inputString)
+        {
+            
+            if (char.IsLetter(character))
+            {
+                Console.Write(character);
+            }
+        }
+
+        Console.WriteLine();
+
+        foreach (char character in inputString)
+        {
+         
+            if (!char.IsDigit(character) && !char.IsLetter(character))
+            {
+                Console.Write(character);
+            }
+        }
+
+        Console.WriteLine(); 
+    }
+}
+
